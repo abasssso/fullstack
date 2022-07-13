a9cb0e5ab9de5c7466e8b0711b8e2e3ba963b43e
abasssso <abylai010201@gmail.com>
2022-07-13 14:44:01 +0600

    added cart rename logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,21 @@ import "./Header.css";
 import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import { useNavigate } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 const Header = () => {
   const navigate = useNavigate();
   return (
     <div className="container">
       <div className="navbar-container">
         <p className="logo" onClick={() => navigate("/home")}>
-          Bikraft
+          Cycle
         </p>
         <div className="navbar-right">
           <p onClick={() => navigate("/products")}>Bicycles</p>
           <p onClick={() => navigate("/insurance")}>Insurance</p>
           <p onClick={() => navigate("/contact")}>Contact</p>
           <PersonIcon onClick={() => navigate("/account")} />
+          <ShoppingCartIcon onClick={() => navigate("/cart")} />
         </div>
       </div>
     </div>
