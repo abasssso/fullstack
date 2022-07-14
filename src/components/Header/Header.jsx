806e516919f9df98801d49819db54cb748a07ae4
abasssso <abylai010201@gmail.com>
2022-07-14 15:22:50 +0600

    responsive footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 import "./Header.css";
+import "./media.css";
 import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import { useNavigate } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const Header = () => {
   const navigate = useNavigate();
   return (
     <div className="container">
       <div className="navbar-container">
-        <p className="logo" onClick={() => navigate("/home")}>
-          Cycle
-        </p>
+        <MenuIcon
+          className="navbar-container_burger"
+          style={{ display: "none" }}
+        />
+        <div className="navbar-container_logo">
+          <h1 onClick={() => navigate("/home")}>Cycle</h1>
+        </div>
         <div className="navbar-right">
           <p onClick={() => navigate("/products")}>Bicycles</p>
           <p onClick={() => navigate("/insurance")}>Insurance</p>
