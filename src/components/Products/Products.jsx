440c0053df32c5b8d307d03cfac58445d514fd14
abasssso <abylai010201@gmail.com>
2022-07-18 15:37:53 +0600

    contact and footer adpative

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Products.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="products-list">
+      <div className="products-list_title">
+        <p style={{ color: "#B2B2B2" }}>Choose the best for you</p>
+        <h1 style={{ display: "flex", fontSize: "64px" }}>
+          our bikes<p style={{ color: "yellow" }}>.</p>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
