c511b6c475017464e3beccee862767a90e65a727
abasssso <abylai010201@gmail.com>
2022-07-21 13:03:57 +0600

    card edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Routing from "./Routing";
 import ProductsContextProvider from "./Contexts/ProductsContext";
 import AuthContextProvider from "./Contexts/AuthContext";
+import CartContextProvider from "./Contexts/CartContext";
 
 function App() {
-    return (
-        <AuthContextProvider>
-            <ProductsContextProvider>
-                <BrowserRouter>
-                    <Header/>
-                    <Routing/>
-                    <Footer/>
-                </BrowserRouter>
-            </ProductsContextProvider>
-        </AuthContextProvider>
-    );
+  return (
+    <CartContextProvider>
+      <AuthContextProvider>
+        <ProductsContextProvider>
+          <BrowserRouter>
+            <Header />
+            <Routing />
+            <Footer />
+          </BrowserRouter>
+        </ProductsContextProvider>
+      </AuthContextProvider>
+    </CartContextProvider>
+  );
 }
 
 export default App;
--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,5 +1,5 @@
-import React, {useContext} from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "./components/Account/Account";
 import Cart from "./components/Cart/Cart";
 import Contact from "./components/Contact/Contact";
@@ -10,35 +10,53 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import RegisterSuccess from "./components/RegisterSuccess/RegisterSuccess";
 import AddProduct from "./components/AddProduct/AddProduct";
-import {authContext} from "./Contexts/AuthContext";
+import { authContext } from "./Contexts/AuthContext";
 import EditProduct from "./components/EditProduct/EditProduct";
 import RestorePass from "./components/RestorePass/RestorePass";
 import RestoreComplete from "./components/RestoreComplete/RestoreComplete";
+import Details from "./components/Details/Details";
 
 const Routing = () => {
-    const {loading, currentUser} = useContext(authContext);
-    if (loading) {
-        return <h1>Loading . . .</h1>;
-    }
-    // console.log(currentUser);
-    return (
-        <Routes>
-            <Route path="/login" element={currentUser ? <Navigate to="products" replace/> : <Login/>}/>
-            <Route path="/restore-pass" element={ <RestorePass/>} />
-            <Route path="/restore-complete" element={ <RestoreComplete/>} />
-            <Route path="/add" element={currentUser ? <AddProduct/> : <Login/>}/>
-            <Route path="/register" element={currentUser ? <Navigate to="/products" replace/> : <Register/>}
-            />
-            <Route path="/register-success" element={currentUser ? (<Navigate to="/products" replace/>) : (<RegisterSuccess/>)}/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/products" element={<Products/>}/>
-            <Route path="/insurance" element={<Insurance/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/account" element={<Account/>}/>
-            <Route path="/cart" element={<Cart/>}/>
-            <Route path="/edit/:id" element={<EditProduct/>}/>
-        </Routes>
-    );
+  const { loading, currentUser } = useContext(authContext);
+  if (loading) {
+    return <h1>Loading . . .</h1>;
+  }
+  // console.log(currentUser);
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={currentUser ? <Navigate to="products" replace /> : <Login />}
+      />
+      <Route path="/restore-pass" element={<RestorePass />} />
+      <Route path="/restore-complete" element={<RestoreComplete />} />
+      <Route path="/add" element={currentUser ? <AddProduct /> : <Login />} />
+      <Route
+        path="/register"
+        element={
+          currentUser ? <Navigate to="/products" replace /> : <Register />
+        }
+      />
+      <Route
+        path="/register-success"
+        element={
+          currentUser ? (
+            <Navigate to="/products" replace />
+          ) : (
+            <RegisterSuccess />
+          )
+        }
+      />
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/insurance" element={<Insurance />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/edit/:id" element={<EditProduct />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  );
 };
 
 export default Routing;
